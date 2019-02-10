ad7ce1edb929dbe66f947be67209e1dfc53ee0f1
mohammed <m.kaddour26@gmail.com>
2019-02-10 15:31:05 -0500

    bug fixed

--- middle-ware/checkAuth.js
+++ middle-ware/checkAuth.js
@@ -2,7 +2,7 @@ let jwt = require("jsonwebtoken")
 
 module.exports= (req,res,next)=>{
     try{
-         let token= req.headers.athuorization.split(" ")[1]
+         let token= req.headers.authorization.split(" ")[1]
     let decoded = jwt.verify(token,"secret")
     req.userData= decoded
 next()
